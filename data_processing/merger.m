6fedc0d7137eaa4e841fcfccb5329506b86f8f3b
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2019-06-16 11:49:44 +0530

    Create  merger.m

---
+++ data_processing/merger.m
@@ -0,0 +1,13 @@
+one = load('final_values_slp37.mat');
+two = load('final_values_slp37_2.mat');
+
+one.energy_mat(77:end,:) = two.energy_mat(77:end,:);
+one.entropy_mat(77:end,:) = two.entropy_mat(77:end,:);
+one.skewness_mat(77:end,:) = two.skewness_mat(77:end,:);
+one.kurtosis_mat(77:end,:) = two.kurtosis_mat(77:end,:);
+energy_mat = one.energy_mat;
+entropy_mat = one.entropy_mat;
+skewness_mat = one.skewness_mat;
+kurtosis_mat = one.kurtosis_mat;
+save('final_values_slp37_merged', 'energy_mat','entropy_mat','skewness_mat','kurtosis_mat');
+
