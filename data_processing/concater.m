7bf32a7d2687550473ba2af0d52fc13b71ad06f3
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2019-06-16 12:22:50 +0530

    Concatenate features and labels.

--- data_processing/concater.m
+++ data_processing/concater.m
@@ -6,3 +6,4 @@ skewness_mat = [one.skewness_mat(21:133,1:6) one.label(1:113)];
 kurtosis_mat = [one.kurtosis_mat(21:133,1:6) one.label(1:113)];
 
 save('slp61.mat', 'energy_mat','entropy_mat','skewness_mat','kurtosis_mat');    
+ 
