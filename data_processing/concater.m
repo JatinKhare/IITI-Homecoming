f9c74969851f864d440d34f4bb876160bd4fa347
Jatin Khare <44063069+JatinKhare@users.noreply.github.com>
2019-06-16 11:51:06 +0530

    Create concater.m

---
+++ data_processing/concater.m
@@ -0,0 +1,8 @@
+one = load('slp61_c.mat');
+
+energy_mat = [one.energy_mat(21:133,1:6) one.label(1:113)];
+entropy_mat = [one.entropy_mat(21:133,1:6) one.label(1:113)];
+skewness_mat = [one.skewness_mat(21:133,1:6) one.label(1:113)];
+kurtosis_mat = [one.kurtosis_mat(21:133,1:6) one.label(1:113)];
+
+save('slp61.mat', 'energy_mat','entropy_mat','skewness_mat','kurtosis_mat');    
